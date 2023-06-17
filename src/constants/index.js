c04cb604e1c0409fee3c9947611205ef8c28b4f1
Sadia Gray <sadiagray17@gmail.com>
2023-06-16 23:50:48 -0500

    completed guided project

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,9 @@
 export const BASE_URL = 'http://localhost:4001'
 export const API_KEY = 'xyz'
+
+
+/*
+Production / Prod  -> Final product -> open to the world!
+Stage              -> This is internal but open to others
+Development / Dev  -> This is you! localhost
+*/
